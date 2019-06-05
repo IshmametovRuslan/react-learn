60e8c51d1a9f7ad6744b1262036448fbc858e000
Ruslan <russ-i@yandex.ru>
2019-06-05 13:18:36 +0300

    components are broken into files

--- projects/01-first-project/my-app-1/src/App.js
+++ projects/01-first-project/my-app-1/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './App.css';
+import TechList from './TechList';
+import Header from './Header';
 
 const App = () => {
 	return (
@@ -9,33 +11,6 @@ const App = () => {
 
 		</div>
 	);
-}
-
-const TechList = () => {
-	return (
-		<div>
-			<h1>Technologies</h1>
-			<ul>
-				<li>Html</li>
-				<li>CSS</li>
-				<li>PHP</li>
-				<li>JS</li>
-			</ul>
-		</div>
-	)
-}
-
-const Header = () => {
-	return (
-		<div>
-			<h1>Это шапка сайта</h1>
-			<ul>
-				<li><a href="#">1</a></li>
-				<li><a href="#">2</a></li>
-				<li><a href="#">3</a></li>
-			</ul>
-		</div>
-);
 }
 
 export default App;
