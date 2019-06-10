2fee7007be9b4bcb1106343c0b12baf0bc86d049
Ruslan <russ-i@yandex.ru>
2019-06-10 14:55:59 +0300

    add dialogs component

--- projects/01-first-project/my-app-1/src/App.js
+++ projects/01-first-project/my-app-1/src/App.js
@@ -1,17 +1,24 @@
 import React from 'react';
-import  './App.css';
-import Header from "./components/Header/Header";
+import './App.css';
+
 import Nav from "./components/Nav/Nav";
 import Main from "./components/Main/Main";
+import Dialogs from "./components/Dialogs/Dialogs";
+import Header from "./components/Header/Header";
+import classes from "./components/Main/Main.module.css";
 
 const App = () => {
-	return (
-		<div className="appWrapper">
-			<Header/>
-			<Nav/>
-			<Main/>
-		</div>
-	);
+    return (
+        <div className="appWrapper">
+            <Header/>
+            <Nav/>
+
+            <div className={classes.main}>
+                <Dialogs/>
+                {/*<Main/>*/}
+            </div>
+        </div>
+    );
 }
 
 export default App;
--- projects/01-first-project/my-app-1/src/components/Main/Main.js
+++ projects/01-first-project/my-app-1/src/components/Main/Main.js
@@ -4,17 +4,17 @@ import ChatBlock from "./ChatBlock/ChatBlock";
 import UserData from "./UserData/UserData";
 
 const Main = () => {
-	return (
-		<div className={classes.main}>
-			<div className={classes.headImage}>
-				<img
-					src="https://natworld.info/wp-content/uploads/2018/01/%D0%A1%D0%BE%D1%87%D0%B8%D0%BD%D0%B5%D0%BD%D0%B8%D0%B5-%D0%BD%D0%B0-%D1%82%D0%B5%D0%BC%D1%83-%D0%9F%D1%80%D0%B8%D1%80%D0%BE%D0%B4%D0%B0-900x500.jpeg"
-					alt=""/>
-			</div>
-			<UserData/>
-			<ChatBlock/>
-		</div>
-	);
+    return (
+        <div>
+            <div className={classes.headImage}>
+                <img
+                    src="https://natworld.info/wp-content/uploads/2018/01/%D0%A1%D0%BE%D1%87%D0%B8%D0%BD%D0%B5%D0%BD%D0%B8%D0%B5-%D0%BD%D0%B0-%D1%82%D0%B5%D0%BC%D1%83-%D0%9F%D1%80%D0%B8%D1%80%D0%BE%D0%B4%D0%B0-900x500.jpeg"
+                    alt=""/>
+            </div>
+            <UserData/>
+            <ChatBlock/>
+        </div>
+    );
 }
 
 export default Main;
